49b6e06fadd0390bc4760a112982de974ad52334
Kuah Hong Xuan <kuahhongxuan@gmail.com>
2025-08-24 23:55:56 +1000

    refactor: updated all auth api links

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -3,26 +3,26 @@ import { apiClient } from "./client"
 
 export const authApi = {
   register: async (data: RegisterPayload): Promise<AuthResponse> => {
-    const response = await apiClient.post<AuthResponse>('/users/register', data);
+    const response = await apiClient.post<AuthResponse>('/api/users/register', data);
     return response.data;
   },
 
   login: async (data: FormData): Promise<AuthResponse> => {
-    const response = await apiClient.post<AuthResponse>('/auth/token', data);
+    const response = await apiClient.post<AuthResponse>('/api/auth/token', data);
     return response.data;
   },
 
   logout: async (): Promise<void> => {
-    await apiClient.post('/auth/logout');
+    await apiClient.post('/api/auth/logout');
   },
 
 
   refreshToken: async (data: RefreshTokenPayload): Promise<AuthResponse> => {
-    const response = await apiClient.post<AuthResponse>('/auth/refresh_token', data);
+    const response = await apiClient.post<AuthResponse>('/api/auth/refresh_token', data);
     return response.data;
   },
 
   refresh: async (): Promise<void> => {
-    await apiClient.post('/auth/refresh_token');
+    await apiClient.post('/api/auth/refresh_token');
   },
 };
