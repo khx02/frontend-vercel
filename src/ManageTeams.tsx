93b0e88fc57ff3009bb585c270018738f005473d
Anthony Kim <akim0023@student.monash.edu>
2025-09-01 12:28:28 +1000

    fix: added gap between teams list view

--- src/ManageTeams.tsx
+++ src/ManageTeams.tsx
@@ -32,47 +32,49 @@ export function ManageTeams() {
       <h1 className="text-3xl font-bold text-left mb-8">Manage Teams</h1>
 
       <h2 className="text-xl font-bold text-left mb-4">My Teams</h2>
-      {teams.map(team => (
-        <Card>
-          <CardContent>
-            <div className="w-full flex flex-row justify-between items-center">
-              <p className="font-semibold">{team.name}</p>
+      <div className="flex flex-col gap-4">
+        {teams.map(team => (
+          <Card>
+            <CardContent>
+              <div className="w-full flex flex-row justify-between items-center">
+                <p className="font-semibold">{team.name}</p>
 
-              <Dialog
-                open={isOpen}
-                onOpenChange={setIsOpen}
-              >
-                <DialogTrigger asChild>
-                  <Button variant="destructive">Leave</Button>
-                </DialogTrigger>
-                <DialogContent>
-                  <DialogTitle>Are you sure?</DialogTitle>
-                  <DialogDescription>
-                    Once you leave you will need to join the team again. Are you sure you
-                    want to leave <span className="font-semibold">{team.name}</span>?
-                  </DialogDescription>
-                  <DialogFooter>
-                    <Button
-                      variant="ghost"
-                      className="text-red-600"
-                      onClick={() => setIsOpen(false)}
-                    >
-                      Cancel
-                    </Button>
-                    <Button
-                      type="button"
-                      onClick={handleLeaveGroup(team.id)}
-                    >
-                      Confirm
-                    </Button>
-                  </DialogFooter>
-                </DialogContent>
-              </Dialog>
+                <Dialog
+                  open={isOpen}
+                  onOpenChange={setIsOpen}
+                >
+                  <DialogTrigger asChild>
+                    <Button variant="destructive">Leave</Button>
+                  </DialogTrigger>
+                  <DialogContent>
+                    <DialogTitle>Are you sure?</DialogTitle>
+                    <DialogDescription>
+                      Once you leave you will need to join the team again. Are you sure you
+                      want to leave <span className="font-semibold">{team.name}</span>?
+                    </DialogDescription>
+                    <DialogFooter>
+                      <Button
+                        variant="ghost"
+                        className="text-red-600"
+                        onClick={() => setIsOpen(false)}
+                      >
+                        Cancel
+                      </Button>
+                      <Button
+                        type="button"
+                        onClick={handleLeaveGroup(team.id)}
+                      >
+                        Confirm
+                      </Button>
+                    </DialogFooter>
+                  </DialogContent>
+                </Dialog>
 
-            </div>
-          </CardContent>
-        </Card>
-      ))}
+              </div>
+            </CardContent>
+          </Card>
+        ))}
+      </div>
 
       <div className="flex justify-around mt-12">
         <CreateTeam
