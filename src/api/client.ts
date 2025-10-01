501c718cc95b6d6f333f493c5cac24f3121ebaf3
Kuah Hong Xuan <hkua0003@student.monash.edu>
2025-10-01 12:52:02 +1000

    host: changed api base url

--- src/api/client.ts
+++ src/api/client.ts
@@ -12,7 +12,7 @@ interface ExtendedAxiosRequestConfig extends InternalAxiosRequestConfig {
   _retry?: boolean;
 }
 
-const API_BASE_URL = "http://localhost:8000/api";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 class ApiClient {
   private axiosInstance: AxiosInstance;
